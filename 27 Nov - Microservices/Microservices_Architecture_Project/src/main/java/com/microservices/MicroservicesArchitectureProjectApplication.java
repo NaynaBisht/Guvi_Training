8b72a3fea8405975a8bb69059954aec5bfaca385
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-11-28 01:06:41 +0530

    Implementing Microservices in Quiz App

---
+++ 27 Nov - Microservices/Microservices_Architecture_Project/src/main/java/com/microservices/MicroservicesArchitectureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesArchitectureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesArchitectureProjectApplication.class, args);
+	}
+
+}
