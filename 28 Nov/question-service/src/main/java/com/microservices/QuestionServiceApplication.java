2515dd02326a6b4b74a41b69f4cfb5ef7f2b2b13
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-11-28 23:36:56 +0530

    Question Microservice setup

---
+++ 28 Nov/question-service/src/main/java/com/microservices/QuestionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.microservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceApplication.class, args);
+	}
+}
