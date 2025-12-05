35c4793468851474256a6d634c5fb1a3c6af0556
Nayna Bisht <145502271+NaynaBisht@users.noreply.github.com>
2025-12-06 01:15:21 +0530

    Egovernance Project added features

---
+++ EgovernanceApp/lib/src/main/java/_EgovernanceApp/CustomerLogin.java
@@ -0,0 +1,20 @@
+package _EgovernanceApp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import default_package.LoggingTest;
+
+public class CustomerLogin {
+	private static final Logger logger = LogManager.getLogger(CustomerLogin.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		logger.debug("Debug Message — For developer analysis");
+		System.out.print("hey there");
+		
+		
+	}
+
+}
