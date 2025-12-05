508f9b82cf5e5b7aea96bd0a53ae9c5573ce70a8
Nayna Bisht <145502271+NaynaBisht@users.noreply.github.com>
2025-12-06 01:14:53 +0530

    Egovernance App

---
+++ EgovernanceProject/lib/src/main/java/_EgovernanceProject/CustomerLogin.java
@@ -0,0 +1,21 @@
+package _EgovernanceProject;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import default_package.LoggingTest;
+
+public class CustomerLogin {
+
+	private static final Logger logger = LogManager.getLogger(CustomerLogin.class);
+
+    public static void main(String[] args) {
+    	
+    	 logger.info("Application started...");
+         logger.debug("Debug log in main()");
+         
+       
+        
+    }
+
+}
