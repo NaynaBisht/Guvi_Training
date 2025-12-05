32fef868489d10756e7b026a9df1f2cd160d527d
Nayna Bisht <145502271+NaynaBisht@users.noreply.github.com>
2025-12-06 01:16:38 +0530

    Comparators - Basics

---
+++ LearnComparators/src/learn_comparator/SortByName.java
@@ -0,0 +1,8 @@
+package learn_comparator;
+import java.util.Comparator;
+
+public class SortByName implements Comparator<Student> {
+	public int compare(Student a, Student b) {
+		return a.name.compareTo(b.name);
+	}
+}
