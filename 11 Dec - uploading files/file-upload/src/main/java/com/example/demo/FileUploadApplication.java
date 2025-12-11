e3e7e92a4e24440f62307260d9d8d3165d8f8c56
Nayna Bisht <145502271+NaynaBisht@users.noreply.github.com>
2025-12-11 16:45:36 +0530

    Spring Boot Rest APIs for uploading Files

---
+++ 11 Dec - uploading files/file-upload/src/main/java/com/example/demo/FileUploadApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import jakarta.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.service.FileStorageService;
+
+
+@SpringBootApplication
+public class FileUploadApplication implements CommandLineRunner {
+	
+	@Resource
+	FileStorageService storageService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+		storageService.init();
+	}
+}
